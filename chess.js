66cae634c6cad7692a5836390cd901cce5ecf8d2
Renu-18 <63661956+Renu-18@users.noreply.github.com>
2020-04-21 23:54:30 +0530

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,218 @@
+var obj=document.querySelector("canvas");
+obj.width=window.innerWidth;
+obj.height=window.innerHeight;
+
+//square
+  //1,2:position,3,4:width,height
+/*
+1.leftorright
+2.up or down
+3.width
+4.height
+*/
+
+//8row
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(100,700,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(300,700,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(500,700,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(700,700,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(900,700,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1100,700,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1300,700,100,100);
+
+//7row
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(0,600,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(200,600,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(400,600,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(600,600,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(800,600,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1000,600,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1200,600,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1400,600,100,100);
+
+
+//6row
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(100,500,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(300,500,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(500,500,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(700,500,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(900,500,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1100,500,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1300,500,100,100);
+
+//5row
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(0,400,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(200,400,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(400,400,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(600,400,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(800,400,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1000,400,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1200,400,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1400,400,100,100);
+
+//4row
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(100,300,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(300,300,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(500,300,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(700,300,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(900,300,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1100,300,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1300,300,100,100);
+
+
+//3row
+
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(0,200,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(200,200,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(400,200,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(600,200,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(800,200,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1000,200,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1200,200,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1400,200,100,100);
+
+
+
+//2row
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(100,100,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(300,100,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(500,100,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(700,100,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(900,100,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1100,100,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1300,100,100,100);
+//1row
+
+var txt=obj.getContext('2d');
+txt.fillStyle="black";  //clr
+txt.fillRect(0,0,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(200,0,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(400,0,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(600,0,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(800,0,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1000,0,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1200,0,100,100);
+var txt=obj.getContext('2d');
+txt.fillStyle="black";
+txt.fillRect(1400,0,100,100);
+
+
+
+
+
